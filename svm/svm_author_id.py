beaf6720e40b4fb1da973fd802c9e191404da55b
Ominini Ambie-Barango <omininiab@users.noreply.github.com>
2017-09-01 16:41:03 -0700

    learn svm through mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
 
 
 ### features_train and features_test are the features for the training
@@ -23,8 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
 
+clf = SVC(C = 10000, kernel = "rbf")
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+prediction = clf.predict(features_test)
+print ("prediction time:", round(time()-t0, 3), "s")
+
+accuracy = clf.score(features_test, labels_test)
+
+chris = sum(x == 1 for x in prediction)
+sara = sum(x == 0 for x in prediction)
+
+print(accuracy, chris, sara)
 #########################################################
-
-
