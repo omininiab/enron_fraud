5c5c221122368cc97a8cb857181f3a7c3639ceeb
Ominini Ambie-Barango <omininiab@users.noreply.github.com>
2017-09-07 23:08:20 -0700

    today's changes

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+def takeLast(elem):
+    return elem[2]
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -7,14 +9,16 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
     cleaned_data = []
 
-    ### your code goes here
+    for i in range(0, len(ages)):
+        cleaned_data.append((ages[i], net_worths[i], abs(predictions[i] - net_worths[i])))
 
-    
+    cleaned_data = sorted(cleaned_data, key=takeLast, reverse=False)
+
+    cleaned_data = cleaned_data[0:int(0.9*len(cleaned_data))]
+
     return cleaned_data
-
