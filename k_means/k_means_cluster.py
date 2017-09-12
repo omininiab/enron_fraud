ea04c1dfb2d54e5c89cb82b898e89dccf39c2e7e
Ominini Ambie-Barango <omininiab@users.noreply.github.com>
2017-09-11 20:11:20 -0700

    more updates

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -50,7 +50,7 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]#, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -60,26 +60,30 @@ for n in range(1,len(features_list)):
     print(feature_n[feature_n > 0].min())
     print(feature_n[feature_n > 0].max())
 
-"""
+
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to
 ### for f1, f2, f3 in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaled_finance_features = scaler.fit_transform(finance_features)
 
 kmeans = KMeans(n_clusters=2)
-pred = kmeans.fit_predict(finance_features)
+pred = kmeans.fit_predict(scaled_finance_features)
 
+print(scaler.transform([[200000.,1000000.]]))
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters3.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_scaled.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print("no predictions object named pred found, no clusters to plot")
-"""
