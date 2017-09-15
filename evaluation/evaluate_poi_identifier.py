220394ec4694d3650ddbcc2bdf531d8cdeb56cd3
Ominini Ambie-Barango <omininiab@users.noreply.github.com>
2017-09-15 11:55:12 -0700

    evaluation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -16,16 +16,30 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_score, recall_score
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier(random_state=0)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+print(precision_score(labels_test, pred))
+print(recall_score(labels_test, pred))
+#print(pred)
+#print(labels_test)
+#print(len(pred[pred==1]))
+#print(clf.score(features_test, labels_test))
